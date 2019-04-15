e21eef1f1611bb152f4d226059cf33e7add40e45
klequis <klequis@gmail.com>
2019-04-15 15:36:30 -0700

    added express

--- index.js
+++ index.js
@@ -1,5 +1,32 @@
 require('dotenv').config()
 
+import express from 'express'
+import bodyParser from 'body-parser'
+import cors from 'cors'
+import morgan from 'morgan'
+import dotenv from 'dotenv'
+
+
 console.log('host:', process.env.DB_HOST)
 console.log('username:', process.env.DB_USER)
 console.log('password:', process.env.DB_PASS)
+
+const app = express()
+
+app.use(cors())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(morgan('dev'))
+
+app.get('/', (req, res) => {
+  res.send('hello')
+})
+
+
+// const port = 3030
+
+app.listen(3030, () => {
+  console.log(`Events API server is listening on port ${3030}`)
+})
+
+export default app
